eeb5a31e9fa84fd7e9bf7635498dcefb1cbf54fa
jiangwei <734145091@qq.com>
2021-03-10 22:18:03 +0800

    医院管理系统

---
+++ appointment-register/hospital-manage/src/main/java/jw/hmanage/hospitalmanage/HospitalManageApplication.java
@@ -0,0 +1,13 @@
+package jw.hmanage.hospitalmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManageApplication.class, args);
+    }
+
+}
