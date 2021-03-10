ecdd0d9a8785bfda79887efbc684c408eea23c2f
jiangwei <734145091@qq.com>
2021-03-10 17:12:33 +0800

    医院设置前端

--- appointment-register/hosptial/src/views/layout/components/AppMain.vue
+++ appointment-register/hosptial/src/views/layout/components/AppMain.vue
@@ -2,8 +2,8 @@
   <section class="app-main">
     <transition name="fade-transform" mode="out-in">
       <!-- or name="fade" -->
-      <!-- <router-view :key="key"></router-view> -->
-      <router-view/>
+       <router-view :key="key"></router-view>
+<!--      <router-view/>-->
     </transition>
   </section>
 </template>
@@ -12,9 +12,9 @@
 export default {
   name: 'AppMain',
   computed: {
-    // key() {
-    //   return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
-    // }
+    key() {
+      return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
+    }
   }
 }
 </script>
